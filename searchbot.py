ad9302a28f8cbca2928b724866776286fcda0e4f
Rishi <88385616+rishik2@users.noreply.github.com>
2021-08-29 16:49:20 +0530

    Add files via upload

---
+++ searchbot.py
@@ -0,0 +1,27 @@
+import tweepy
+import time
+
+consumer_key = '3LbhIolwXUUZPiRTR9okVfo1k'
+consumer_secret = 'bQMuIQa1y4bQuhRjn3B5ulH0gdrfrCxfqAzEqcYoBFRIkugu4K'
+key = '1429350004943065089-hIAC68H77KAAHU4rh2ofnkFBmbL11Q'
+secret = 'LL6veJbobNf1DqIpx46yy6gZV72ysCzQUFcFBbK4VywSV'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(key, secret)
+
+api = tweepy.API(auth)
+hashtag = ("#python", '#100daysofcoding', '#java')
+tweetnumber = 3
+tweets = tweepy.Cursor(api.search, hashtag).items(tweetnumber)
+
+def retweet_hashtag():
+    for tweet in tweets:
+        try:
+            tweet.retweet()
+            print("Retweet Successfull")
+            time.sleep(10)
+        except tweepy.TweepError as e:
+            print(e.reason)
+            time.sleep(10)
+
+retweet_hashtag()
---
+++ twbot.py
@@ -0,0 +1,48 @@
+import tweepy
+import time
+
+consumer_key = '3LbhIolwXUUZPiRTR9okVfo1k'
+consumer_secret = 'bQMuIQa1y4bQuhRjn3B5ulH0gdrfrCxfqAzEqcYoBFRIkugu4K'
+key = '1429350004943065089-hIAC68H77KAAHU4rh2ofnkFBmbL11Q'
+secret = 'LL6veJbobNf1DqIpx46yy6gZV72ysCzQUFcFBbK4VywSV'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(key, secret)
+
+api = tweepy.API(auth)
+
+
+
+FILENAME = 'last_seen_id.txt'
+
+def last_seen_id(FILENAME):
+    read_file = open(FILENAME, 'r' )
+    last_seen_id = int(read_file.read().strip())
+    read_file.close()
+    return last_seen_id
+
+def store_last_seen(FILENAME, last_seen_id):
+    write_file = open(FILENAME, 'w')
+    write_file.write(str(last_seen_id))
+    write_file.close()
+
+
+
+
+def reply():
+    tweets = api.mentions_timeline(last_seen_id(FILENAME), tweet_mode="extended")
+    for tweet in reversed(tweets):
+        if '#randomtweet' in tweet.full_text.lower():
+            print("Replied to tweet id: "  +  str(tweet.id))
+            api.update_status( '@' + tweet.user.screen_name +  " Thanks #100 Days of Coding " + str(tweet.id),  tweet.id )
+            api.create_favorite(tweet.id)
+            api.retweet(tweet.id)
+            store_last_seen(FILENAME ,tweet.id)
+        
+while True:    
+    reply()
+    time.sleep(120)
+    print("Working...")
+
+    
+            
